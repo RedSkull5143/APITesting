165527150611730143411c560ddd046d72a6efe2
RedSkull <omshinde5143@gmail.com>
2024-07-09 16:36:45 +0530

    Asserts the body content

---
+++ src/main/java/com/omshinde/CheckBody.java
@@ -0,0 +1,36 @@
+package com.omshinde;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class CheckBody {
+    public static void main(String[] args) {
+        //given - all ips
+        //when - api (resoource&http)
+        //then -
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+        given().log().all().queryParam("key","qaclick123")
+                .header("Content-Type","application/json")
+                .body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": 19.1043529,\n" +
+                        "    \"lng\": 73.009023\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"62 63 Balaji Shradha\",\n" +
+                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "  \"address\": \"sector 5, kopar khairane\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://google.com\",\n" +
+                        "  \"language\": \"marathi\"\n" +
+                        "}\n")
+                .when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200)
+                .body("scope",equalTo("APP"));
+    }
+}
